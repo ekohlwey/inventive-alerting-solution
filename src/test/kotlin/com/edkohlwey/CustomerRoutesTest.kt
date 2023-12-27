d0212bd66902dcf70dcba3a616b19e9947edb92f
Edmund Kohlwey <ekohlwey@gmail.com>
2023-12-27 08:33:17 -0500

    Refactor Customer entity before adding additional entities

--- src/test/kotlin/com/edkohlwey/CustomerRoutesTest.kt
+++ src/test/kotlin/com/edkohlwey/CustomerRoutesTest.kt
@@ -2,26 +2,28 @@ package com.edkohlwey
 
 import com.edkohlwey.plugins.*
 import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
-import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
 import io.ktor.util.reflect.*
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
-class ApplicationTest {
+class CustomerRoutesTest {
     @Test
     fun missingCustomerReturnsNotFound() = testApplication {
         val client = createClient {
-            install(ContentNegotiation){
+            install(ContentNegotiation) {
                 json()
             }
         }
         application {
-            configureDatabases()
             configureSerialization()
+            val database = configureDatabases()
+            configureCustomerRoutes(database)
         }
         client.get("/customers/test-customer").apply {
             assertEquals(HttpStatusCode.NotFound, status)
@@ -32,24 +34,25 @@ class ApplicationTest {
     @Test
     fun customerCanBeAdded() = testApplication {
         val client = createClient {
-            install(ContentNegotiation){
+            install(ContentNegotiation) {
                 json()
             }
         }
         application {
-            configureDatabases()
             configureSerialization()
+            val database = configureDatabases()
+            configureCustomerRoutes(database)
         }
-        client.post("/customers"){
+        client.post("/customers") {
             contentType(ContentType.Application.Json)
-            setBody(CreateCustomerRequest(name="test-customer"))
+            setBody(CreateCustomerRequest(name = "test-customer"))
         }.apply {
             assertEquals(HttpStatusCode.Created, status)
             assertEquals("", bodyAsText())
         }
         client.get("/customers/test-customer").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals(GetCustomerResponse(name="test-customer"), body(typeInfo<GetCustomerResponse>()))
+            assertEquals(GetCustomerResponse(name = "test-customer"), body(typeInfo<GetCustomerResponse>()))
         }
     }
 }
